bee7d55b23f46a93d4c962443ecbb09770277dc9
Sharada Shivanand Mallya <sharada.s.mallya@kipi.bi>
2024-07-22 12:46:11 +0530

    new commit filter

--- filter.py
+++ filter.py
@@ -1,12 +1,4 @@
-# The Snowpark package is required for Python Worksheets. 
-# You can add more packages by selecting them using the Packages control and then importing them.
-
-import snowflake.snowpark as snowpark
 from snowflake.snowpark.functions import col
-
-def main(session: snowpark.Session): 
-    # Your code goes here, inside the "main" handler.
-   from snowflake.snowpark.functions import col
 
 def filter_by_role(session, table_name, role):
   df = session.table(table_name)
